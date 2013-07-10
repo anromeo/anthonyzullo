5176efc36f35d6fd09f09b17a9d2dc7c208d0b42
anromeo <anthonykzullo@gmail.com>
2013-07-10 15:27:44 -0700

    added readme.md

---
+++ anthonyzullo/README.md
@@ -0,0 +1,4 @@
+Anthony Zullo's Personal Site
+============================
+
+This is a personal site that is dedicated to my different projects that I do in my spare time and lists some of my works in progress.
---
+++ anthonyzullo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anthonyzullo::Application.config.secret_key_base = 'eea59d79eabd0a111f25f83372e3442653293e6b77f975705a90a147a1450abc168545f1a463d9c673656112195c2c541f429d74208edbcfdc065639c96900ab'
